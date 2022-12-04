6b8b796638bfebeccf1d0b411b7b2ff40de53348
a.barai <a.barai6003@gmail.com>
2022-12-04 19:02:17 +0300

    AddDist

--- Projects/Pulsometer/src/js/script.js
+++ Projects/Pulsometer/src/js/script.js
@@ -1,139 +1,141 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        autoplay: true,
-        autoplaySpeed: 3000,
-        speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="assets/icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="assets/icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-                
-            }
-        ]
-    });
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1200,
+    prevArrow: '<button type="button" class="slick-prev"><img src="assets/icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="assets/icons/right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false
+        }
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-      });
-
-      function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            })
-          })
       }
-
-      toggleSlide('.catalog-item__link');
-      toggleSlide('.catalog-item__back');
-
-      // Modal
-
-      $('[data-modal=consultation]').on('click', function(){
-            $('.overlay, #consultation').fadeIn('slow');
-      });
-
-      $('.modal__close').on('click', function(){
-        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    ]
   });
 
-$('.button_mini').each(function(i){
-    $(this).on('click', function(){
-       $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-       $('.overlay, #order').fadeIn('slow'); 
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
     })
-});
+  }
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+  // Modal
+
+  $('[data-modal=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+  });
+
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    })
+  });
 
 
-function validation_form(form) {
+  function validation_form(form) {
     $(form).validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            email: {
-              required: true,
-              email: true
-            },
-            phone: {
-                required: true
-            }
-          },
-    
-          messages: {
-            name: {
-                required: "Please enter your name",
-                minlength: jQuery.validator.format("At least {0} characters required!")   
-            },
-                
-            phone: {
-              required: "Please enter your phone",
-            },
-            email: {
-              required: "We need your email to contact you",
-              email: "Your email address must be in the format of name@domain.com"
-            }
-          }
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        phone: {
+          required: true
+        }
+      },
+
+      messages: {
+        name: {
+          required: "Please enter your name",
+          minlength: jQuery.validator.format("At least {0} characters required!")
+        },
+
+        phone: {
+          required: "Please enter your phone",
+        },
+        email: {
+          required: "We need your email to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        }
+      }
     });
-}
+  }
 
-validation_form('#consultation-form');
-validation_form('#consultation form');
-validation_form('#order form');
+  validation_form('#consultation-form');
+  validation_form('#consultation form');
+  validation_form('#order form');
 
 
-$('input[name=phone]').mask("+999 (99) 999-99-99");
+  $('input[name=phone]').mask("+999 (99) 999-99-99");
 
-$('form').submit(function(e) {
-    if(!$(this).valid()){
-        return;
+  $('form').submit(function (e) {
+    if (!$(this).valid()) {
+      return;
     }
     e.preventDefault();
     $.ajax({
-        // client-server settings such as type, url, date 
-    }).done(function() {
-        // after information is send we clear inputs and value
-        $(this).find("input").val("");
-        $('#consultation, #order').fadeOut();
-        $('.overlay, #thanks').fadeIn('slow');
-        $('form').trigger('reset');
+      // client-server settings such as type, url, date 
+    }).done(function () {
+      // after information is send we clear inputs and value
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+      $('form').trigger('reset');
     });
     return false;
-});
+  });
 
-// Smooth scroll up
-$(window).scroll(function(){
-    if($(this).scrollTop() > 1600 ){
-        $('.pageup').fadeIn();
+  // Smooth scroll up
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
     } else {
-        $('.pageup').fadeOut();
+      $('.pageup').fadeOut();
     }
-        
-});
 
-$("#up").on('click', function(event) {
+  });
+
+  $("a[href=#up").on('click', function (event) {
     if (this.hash !== "") {
       event.preventDefault();
 
       const hash = this.hash;
+
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 800, function () {
+
         window.location.hash = hash;
       });
     }
   });
 
-new WOW().init();
+  new WOW().init();
 
 });
